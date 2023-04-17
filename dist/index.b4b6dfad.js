a6bc85976a6f38befd79f5cdf2212aed50125d08
KunKang82 <kunzsayz@hotmail.com>
2023-04-17 00:40:48 -0400

    3.5 final

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -28504,14 +28504,14 @@ const LoginView = ({ onLoggedIn  })=>{
                         onChange: (e)=>setUsername(e.target.value)
                     }, void 0, false, {
                         fileName: "src/components/login-view/login-view.jsx",
-                        lineNumber: 65,
-                        columnNumber: 9
+                        lineNumber: 64,
+                        columnNumber: 5
                     }, undefined)
                 ]
             }, void 0, true, {
                 fileName: "src/components/login-view/login-view.jsx",
-                lineNumber: 63,
-                columnNumber: 7
+                lineNumber: 62,
+                columnNumber: 4
             }, undefined),
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("label", {
                 children: [
@@ -28522,28 +28522,28 @@ const LoginView = ({ onLoggedIn  })=>{
                         onChange: (e)=>setPassword(e.target.value)
                     }, void 0, false, {
                         fileName: "src/components/login-view/login-view.jsx",
-                        lineNumber: 73,
-                        columnNumber: 9
+                        lineNumber: 72,
+                        columnNumber: 5
                     }, undefined)
                 ]
             }, void 0, true, {
                 fileName: "src/components/login-view/login-view.jsx",
-                lineNumber: 71,
-                columnNumber: 7
+                lineNumber: 70,
+                columnNumber: 4
             }, undefined),
             /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("button", {
                 type: "submit",
                 children: "Submit"
             }, void 0, false, {
                 fileName: "src/components/login-view/login-view.jsx",
-                lineNumber: 79,
-                columnNumber: 7
+                lineNumber: 78,
+                columnNumber: 4
             }, undefined)
         ]
     }, void 0, true, {
         fileName: "src/components/login-view/login-view.jsx",
-        lineNumber: 62,
-        columnNumber: 5
+        lineNumber: 61,
+        columnNumber: 3
     }, undefined);
 };
 _s(LoginView, "Lrw7JeD9zj6OUWhT/IH4OIvPKEk=");
--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -3,12 +3,8 @@ import { useState } from "react";
 export const LoginView = ({ onLoggedIn }) => {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
-	const [password, setPassword] = useState("");
 
 	// Validation of user login
-	const handleSubmit = (event) => {
-		// this prevents the default behavior of the form which is to reload the entire page
-		event.preventDefault();
 	const handleSubmit = (event) => {
 		// this prevents the default behavior of the form which is to reload the entire page
 		event.preventDefault();
@@ -20,13 +16,9 @@ export const LoginView = ({ onLoggedIn }) => {
 
 		// fetch("https://openlibrary.org/account/login.json", {
 		//   method: "POST",
-		// fetch("https://openlibrary.org/account/login.json", {
-		//   method: "POST",
 		// 	headers: {
 		// 		"Content-Type": "application/json"
 		// 	},
-		//   body: JSON.stringify(data)
-		// }).then((response) => {
 		//   body: JSON.stringify(data)
 		// }).then((response) => {
 		// 	if (response.ok) {
@@ -59,27 +51,12 @@ export const LoginView = ({ onLoggedIn }) => {
 				} else {
 					alert("No such user");
 				}
-				if (data.user) {
-					localStorage.setItem("user", JSON.stringify(data.user));
-					localStorage.setItem("token", data.token);
-					onLoggedIn(data.user, data.token);
-				} else {
-					alert("No such user");
-				}
 			})
-			.catch((e) => {
-				alert("Something went wrong");
-			});
 			.catch((e) => {
 				alert("Something went wrong");
 			});
 	};
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<label>
-				Username:
-				<input
 	return (
 		<form onSubmit={handleSubmit}>
 			<label>
@@ -93,18 +70,10 @@ export const LoginView = ({ onLoggedIn }) => {
 			<label>
 				Password:
 				<input
-			</label>
-			<label>
-				Password:
-				<input
 					type="password"
 					value={password}
 					onChange={(e) => setPassword(e.target.value)}
 				/>
-			</label>
-			<button type="submit">Submit</button>
-		</form>
-	);
 			</label>
 			<button type="submit">Submit</button>
 		</form>
